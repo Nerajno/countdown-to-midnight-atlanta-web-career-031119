d041c2f5a96ba0486ddb4c248673f18f175da5b8
active-object-6703 <nerajno@gmail.com>
2019-01-20 21:17:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,6 @@
 #write your code here
-def countdown_with_sleep
+def countdown_with_sleep(number)
   sleep 5
-
 end
   
 
@@ -9,7 +8,7 @@ end
 def countdown(number)
   while number > 0
   puts "#{number} SECOND(S)!"
-
+  countdown_with_sleep(number)
   number -=1
 end
 return "HAPPY NEW YEAR!"
